26e0d16abb70ac596ee6dec349e9a200c83a16d4
ABLEOFFICIAL <ekwesijoshua@gmail.com>
2024-11-17 14:57:19 -0800

    weight converter

--- script.js
+++ script.js
@@ -11,21 +11,130 @@ var section = document.getElementById('section');
 
 
 
-from.addEventListener('input', function(){
-    var fromValue = parseFloat(from.value);
-    var toValue = to.value;
+from.addEventListener('input', function() {
+    var fromValue = parseFloat(from.value);//ensures that the value is treated as a number for calculation.
+    var toValue;
 
-    if(from.placeholder === 'kilogram' && to.placeholder === 'gram'){
+    if (from.placeholder === 'Kilogram' && to.placeholder === 'Gram') {
         toValue = fromValue * 1000;
+        to.value = toValue;
+    }else if (from.placeholder === 'Kilogram' && to.placeholder === 'Pounds') {
+        toValue = fromValue * 2.20462;
+        to.value = toValue;
+    }else if (from.placeholder === 'Kilogram' && to.placeholder === 'Kilogram') {
+        toValue = fromValue;
+        to.value = toValue;
+    }else if (from.placeholder === 'Kilogram' && to.placeholder === 'Milligram') {
+        toValue = fromValue * 1000000;
+        to.value = toValue;
+    }else if (from.placeholder === 'Gram' && to.placeholder === 'Gram') {
+        toValue = fromValue;
+        to.value = toValue;
+    }else if (from.placeholder === 'Gram' && to.placeholder === 'Kilogram') {
+        toValue = fromValue / 1000;
+        to.value = toValue;
+    }else if (from.placeholder === 'Gram' && to.placeholder === 'Milligram') {
+        toValue = fromValue * 1000;
+        to.value = toValue;
+    }else if (from.placeholder === 'Gram' && to.placeholder === 'Pounds') {
+        toValue = fromValue * 0.00220462;
+        to.value = toValue;
+    }else if (from.placeholder === 'Milligram' && to.placeholder === 'Gram') {
+        toValue = fromValue / 1000;
+        to.value = toValue;
+    }else if (from.placeholder === 'Milligram' && to.placeholder === 'Kilogram') {
+        toValue = fromValue / 1000000;
+        to.value = toValue;
+    }else if (from.placeholder === 'Milligram' && to.placeholder === 'Milligram') {
+        toValue = fromValue;
+        to.value = toValue;
+    }else if (from.placeholder === 'Milligram' && to.placeholder === 'Pounds') {
+        toValue = fromValue / 453592;
+        to.value = toValue;
+    }else if (from.placeholder === 'Pounds' && to.placeholder === 'Gram') {
+        toValue = fromValue / 0.00220462;
+        to.value = toValue;
+    }else if (from.placeholder === 'Pounds' && to.placeholder === 'Kilogram') {
+        toValue = fromValue / 2.20462;
+        to.value = toValue;
+    }else if (from.placeholder === 'Pounds' && to.placeholder === 'Milligram') {
+        toValue = fromValue * 453592;
+        to.value = toValue;
+    }else if (from.placeholder === 'Pounds' && to.placeholder === 'Pounds') {
+        toValue = fromValue;
+        to.value = toValue;
     }
-    // to.value = fromInput * 1000;
-    
-
 })
 
-// from.addEventListener('click', toggleMassItem);
+to.addEventListener('input', function(){
+    var toValue = parseFloat(to.value);
+    var fromValue;
+
+    if(to.placeholder === 'Gram' && from.placeholder === 'Gram'){
+        fromValue = toValue;
+        from.value = fromValue;
+    }else if(to.placeholder === 'Gram' && from.placeholder === 'Kilogram'){
+        fromValue = toValue / 1000;
+        from.value = fromValue;
+    }else if(to.placeholder === 'Gram' && from.placeholder === 'Milligram'){
+        fromValue = toValue * 1000;
+        from.value = fromValue;
+    }else if(to.placeholder === 'Gram' && from.placeholder === 'Pounds'){
+        fromValue = toValue * 0.00220462;
+        from.value = fromValue;
+    }else if(to.placeholder === 'Kilogram' && from.placeholder === 'Gram'){
+        fromValue = toValue * 1000;
+        from.value = fromValue;
+    }else if(to.placeholder === 'Kilogram' && from.placeholder === 'Kilogram'){
+        fromValue = toValue;
+        from.value = fromValue;
+    }else if(to.placeholder === 'Kilogram' && from.placeholder === 'Milligram'){
+        fromValue = toValue * 1000000;
+        from.value = fromValue;
+    }else if(to.placeholder === 'Kilogram' && from.placeholder === 'Pounds'){
+        fromValue = toValue * 2.20462;
+        from.value = fromValue;
+    }else if(to.placeholder === 'Milligram' && from.placeholder === 'Gram'){
+        fromValue = toValue / 1000;
+        from.value = fromValue;
+    }else if(to.placeholder === 'Milligram' && from.placeholder === 'Kilogram'){
+        fromValue = toValue / 1000000;
+        from.value = fromValue;
+    }else if(to.placeholder === 'Milligram' && from.placeholder === 'Milligram'){
+        fromValue = toValue;
+        from.value = fromValue;
+    }else if(to.placeholder === 'Milligram' && from.placeholder === 'Pounds'){
+        fromValue = toValue / 453592;
+        from.value = fromValue;
+    }else if(to.placeholder === 'Pounds' && from.placeholder === 'Gram'){
+        fromValue = toValue / 0.00220462;
+        from.value = fromValue;
+    }else if(to.placeholder === 'Pounds' && from.placeholder === 'Kilogram'){
+        fromValue = toValue / 2.20462;
+        from.value = fromValue;
+    }else if(to.placeholder === 'Pounds' && from.placeholder === 'Milligram'){
+        fromValue = toValue * 453592;
+        from.value = fromValue;
+    }else if(to.placeholder === 'Pounds' && from.placeholder === 'Pounds'){
+        fromValue = toValue;
+        from.value = fromValue;
+    }
+})
+
+// event for clearing fields
+from.addEventListener('input', removeField);
+to.addEventListener('input', removeField);
+
+function removeField(e){
+    if (from.value === '' || to.value === '') {
+        from.value = '';
+        to.value = '';
+    }
+}
+
+// event to toggle list item
 arrow.addEventListener('click', toggleMassItem);
-// to.addEventListener('click', toggleMassItem2);
+
 arrow2.addEventListener('click', toggleMassItem2);
 
 
